e5ebdf120d8d5bbd262b02da943c03afb9c5b4fd
Brady <brady.withers@outlook.com>
2018-09-17 11:41:41 -0600

    maps changes

---
+++ assets/js/maps.js
@@ -0,0 +1,15 @@
+var AboveAndBeyond = {
+	lat: 41.0996676,
+	lng: -74.0127113
+};
+
+var AceSewing = {
+	lat: 45.3030876,
+	lng: -122.9582768
+};
+
+var AndersonSewingCenter = {
+	lat:  50.675931,
+	lng: -120.3396706
+};
+
