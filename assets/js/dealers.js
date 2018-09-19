5d2f7f5aa5ae3524d8743e9fbd35c08b143a7f77
Brady Withers <brady.withers@outlook.com>
2018-09-18 21:28:29 -0600

    dynamically creating dealers on dealers.html

--- assets/js/dealers.js
+++ assets/js/dealers.js
@@ -1,20 +1,35 @@
-$(document).ready(function () {
-	$.getJSON("../assets/data/dealers.json", function (data) {
-			//I can see all the individual arrays within the JSON file.
-			console.log(data.name);
-			console.log(data.address);
-			console.log(data.phone_number);
-			//showing basic data from JSON file is working.
-			var applicant_data = '';
-			//.each loop over the data from data.json
-			$.each(data.applicants, function (key, value) {
-					applicant_data += '<tr>';
-					applicant_data += '<td>' + value.name + '</td>';
-					applicant_data += '<td>' + value.address + '</td>';
-					applicant_data += '<td>' + value.phone_nubmer + '</td>';
-					applicant_data += '</tr>';
-			});
-			$('#applicant_table').append(applicant_data);
-			//WORKING ON JOINING / SORTING / MERGING DATA FIELDS??
-	});
-});
+var dealers = [
+{name: 'sewing company', 
+address: 'sewing company lane',
+phoneNumber: '1800-523-1235'},
+
+{name: 'sewing company', 
+address: 'sewing company lane',
+phoneNumber: '1800-523-1235'},
+
+{name: 'sewing company', 
+address: 'sewing company lane',
+phoneNumber: '1800-523-1235'},
+
+{name: 'sewing company', 
+address: 'sewing company lane',
+phoneNumber: '1800-523-1235'},
+
+{name: 'sewing company', 
+address: 'sewing company lane',
+phoneNumber: '1800-523-1235'},
+
+{name: 'sewing company', 
+address: 'sewing company lane',
+phoneNumber: '1800-523-1235'}
+];
+	
+var text = "<div class='container col-md-7 col-sm-7 col-lg-7'><h2 class='text-center'>LockSmith Lizzie Dealers</h2><table class ='table table-striped table-bordered table-hover'><tr><th>Name</th><th>Address</th><th>Phone Number</th></tr></div>"
+
+
+
+for (var i=0;i<dealers.length;i++){
+ text+="<tr><td>"+dealers[i].name+"</td><td>"+dealers[i].address+"</td><td>"+dealers[i].phoneNumber+"</td></tr>"				
+}
+text+="</table>";
+document.write(text);
