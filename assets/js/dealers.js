d133109bcf47cc76ac3ad4d80517f351f6f09221
Brady Withers <brady.withers@outlook.com>
2018-09-18 21:39:37 -0600

    dealers.js object changes and styling

--- assets/js/dealers.js
+++ assets/js/dealers.js
@@ -1,35 +1,48 @@
-var dealers = [
-{name: 'sewing company', 
-address: 'sewing company lane',
-phoneNumber: '1800-523-1235'},
+var dealers = [{
+		name: 'Above and Beyond',
+		address: '38 N Middletown Rd Nanuet, NY 10954',
+		phoneNumber: '1-845-623-4313'
+	},
 
-{name: 'sewing company', 
-address: 'sewing company lane',
-phoneNumber: '1800-523-1235'},
+	{
+		name: 'Ace Sewing and Vacuum',
+		address: '2414 Portland Rd Suite 1 Newberg, OR 97132',
+		phoneNumber: '1-503-538-1122'
+	},
 
-{name: 'sewing company', 
-address: 'sewing company lane',
-phoneNumber: '1800-523-1235'},
+	{
+		name: 'sewing company',
+		address: 'sewing company lane',
+		phoneNumber: '1800-523-1235'
+	},
 
-{name: 'sewing company', 
-address: 'sewing company lane',
-phoneNumber: '1800-523-1235'},
+	{
+		name: 'sewing company',
+		address: 'sewing company lane',
+		phoneNumber: '1800-523-1235'
+	},
 
-{name: 'sewing company', 
-address: 'sewing company lane',
-phoneNumber: '1800-523-1235'},
+	{
+		name: 'sewing company',
+		address: 'sewing company lane',
+		phoneNumber: '1800-523-1235'
+	},
 
-{name: 'sewing company', 
-address: 'sewing company lane',
-phoneNumber: '1800-523-1235'}
+	{
+		name: 'sewing company',
+		address: 'sewing company lane',
+		phoneNumber: '1800-523-1235'
+	}
 ];
-	
-var text = "<div class='container col-md-7 col-sm-7 col-lg-7'><h2 class='text-center'>LockSmith Lizzie Dealers</h2><table class ='table table-striped table-bordered table-hover'><tr><th>Name</th><th>Address</th><th>Phone Number</th></tr></div>"
+
+var info = "<div class='container col-md-7 col-sm-7 col-lg-7'> <h2 class='text-center'>LockSmith Lizzie Dealers</h2> <table class ='table table-striped table-bordered table-hover'> <tr> <th class='text-center'>Name</th> <th class='text-center'>Address</th> <th class='text-center'>Phone Number</th> </tr> </div>"
 
 
 
-for (var i=0;i<dealers.length;i++){
- text+="<tr><td>"+dealers[i].name+"</td><td>"+dealers[i].address+"</td><td>"+dealers[i].phoneNumber+"</td></tr>"				
+for (var i = 0; i < dealers.length; i++) {
+	info += "<tr><td>" + dealers[i].name + "</td><td>" + dealers[i].address + "</td><td>" + dealers[i].phoneNumber + "</td></tr>"
 }
-text+="</table>";
-document.write(text);
+
+info += "</table>";
+
+document.write(info);
