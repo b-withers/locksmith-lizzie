3ac088c041c5c1769071a072031f07b92603cf53
Brady Withers <brady.withers@outlook.com>
2018-09-20 20:18:05 -0600

    adding to dealers page

--- assets/js/dealers.js
+++ assets/js/dealers.js
@@ -29,9 +29,21 @@ var dealers = [{
 	},
 
 	{
-		name: 'sewing company',
-		address: 'sewing company lane',
-		phoneNumber: '1800-523-1235'
+		name: 'Electric Hospital',
+		address: '988 S Broadway Coos Bay, OR 97420',
+		phoneNumber: '1-541-267-2241'
+	},
+
+	{
+		name: 'Orgeon Quilt Woman',
+		address: '880 Railroad Ave Ontario OR 97914',
+		phoneNumber: '1-541-881-1160'
+	},
+
+	{
+		name: 'Pieced Together Studio',
+		address: '205 W Main Mesquite, TX 75149',
+		phoneNumber: '1-972-270-0961'
 	}
 ];
 
@@ -48,33 +60,65 @@ document.write(info);
 
 
 var dealerscord = [{
-	name: "Above and Beyond",
-	cords: {
-		lat: 41.0996676,
-		lng: -74.0127113
-	}
-},
+		name: "Above and Beyond",
+		cords: {
+			lat: 41.0996676,
+			lng: -74.0127113
+		}
+	},
 
-{
-	name: "Ace Sewing Center",
-	cords: {
-		lat: 45.3030876,
-		lng: -122.9582768
-	}
-},
+	{
+		name: "Ace Sewing and Vacuum",
+		cords: {
+			lat: 45.3030876,
+			lng: -122.9582768
+		}
+	},
 
-{
-	name: "Anderson Sewing Center",
-	cords: {
-		lat: 50.675931,
-		lng: -120.3396706
-	}
-},
-{
-	name: "Clay's Bachata Haus",
-	cords: {
-		lat: 47.675931,
-		lng: -100.3396706
-	}
-},
+	{
+		name: "Anderson Sewing Center",
+		cords: {
+			lat: 50.675931,
+			lng: -120.3396706
+		}
+	},
+	{
+		name: "Bearly Threaded Quilting",
+		cords: {
+			lat: 60.516219,
+			lng: -151.1472738
+		}
+
+	}, {
+		name: "Bob's Sewing and Vacuum",
+		cords: {
+			lat: 35.2073876,
+			lng: -106.6517018
+		}
+	},
+
+	{
+		name: "Electric Hospital",
+		cords: {
+			lat: 43.3582576,
+			lng: -124.2151345
+		}
+	},
+
+	{
+		name: "Oregon Quilt Woman",
+		cords: {
+			lat: 44.001451,
+			lng: -117.0830477
+		}
+	},
+
+	{
+		name: "Pieced Together Studio",
+		cords: {
+			lat: 32.767008,
+			lng: -96.6026727
+		}
+	},
+
 ];
