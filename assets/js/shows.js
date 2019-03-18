b503ab1e08b04d445d1c6821e5c1cc24ce9ebfc3
Brady <brady.withers@outlook.com>
2019-03-18 17:37:25 -0600

    updated shows section

--- assets/js/shows.js
+++ assets/js/shows.js
@@ -1,7 +1,23 @@
-var shows = [{
+var shows = [
+	{
 	show: 'Utah Quilting and Sewing Marketplace',
 	website: "<a href='https://www.downunderdiversions.com/uqsm-show.htm' target='_blank'>click here</a>",
 	date: 'May 1-4, 2019'
+},
+{
+	show: 'Quiltfest Lancaster',
+	website: "<a href='https://www.quiltfest.com/upcoming-shows-and-events/quiltfest-lancaster/' target='_blank'>click here</a>",
+	date: 'May 9-11, 2019'
+},
+{
+	show: 'Vermont Quilt Fest',
+	website: "<a href='https://www.vqf.org/home/' target='_blank'>click here</a>",
+	date: 'June 26-29, 2019'
+},
+{
+	show: 'World Quilt New England',
+	website: "<a href='https://www.quiltfest.com/upcoming-shows-and-events/world-quilt-new-england/' target='_blank'>click here</a>",
+	date: 'August 15-17, 2019'
 },
 ];
 
